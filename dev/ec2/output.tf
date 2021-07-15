ee8fe5c8962fed0077a8b58767a92840aeb053ec
codebind26 <64844675+codebind26@users.noreply.github.com>
2021-07-15 19:02:06 +0530

    creating module files

---
+++ dev/ec2/output.tf
@@ -0,0 +1,5 @@
+output "id" {
+  description = "List of IDs of instances"
+  value       = aws_instance.myec2.id
+}
+
---
+++ dev/ec2/variables.tf
@@ -0,0 +1,9 @@
+variable "ami" {
+  description = "ID of AMI to use for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+}
