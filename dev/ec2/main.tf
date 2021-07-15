6169f4cfa34352c5371f6b22b9bc6b754d2070d5
codebind26 <64844675+codebind26@users.noreply.github.com>
2021-07-15 19:28:06 +0530

    Update main.tf

--- dev/ec2/main.tf
+++ dev/ec2/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-west-2"
+    profile = "terragrunt-demo"
+}
 resource "aws_instance" "myec2" {
   ami = var.ami
   instance_type = var.instance_type
